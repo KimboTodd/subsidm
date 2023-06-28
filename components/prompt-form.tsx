ad44ac1cc022bad7856a9e8a8999752a983d03bd
KimboTodd <kimtoddsotheremail@gmail.com>
2023-06-28 16:52:52 -0400

    prompt form max heigh

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -1,53 +1,53 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Textarea from 'react-textarea-autosize'
-import { UseChatHelpers } from 'ai/react'
+import * as React from 'react';
+import Link from 'next/link';
+import Textarea from 'react-textarea-autosize';
+import { UseChatHelpers } from 'ai/react';
 
-import { useEnterSubmit } from '@/lib/hooks/use-enter-submit'
-import { cn } from '@/lib/utils'
-import { Button, buttonVariants } from '@/components/ui/button'
+import { useEnterSubmit } from '@/lib/hooks/use-enter-submit';
+import { cn } from '@/lib/utils';
+import { Button, buttonVariants } from '@/components/ui/button';
 import {
   Tooltip,
   TooltipContent,
-  TooltipTrigger
-} from '@/components/ui/tooltip'
-import { IconArrowElbow, IconPlus } from '@/components/ui/icons'
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { IconArrowElbow, IconPlus } from '@/components/ui/icons';
 
 export interface PromptProps
   extends Pick<UseChatHelpers, 'input' | 'setInput'> {
-  onSubmit: (value: string) => void
-  isLoading: boolean
+  onSubmit: (value: string) => void;
+  isLoading: boolean;
 }
 
 export function PromptForm({
   onSubmit,
   input,
   setInput,
-  isLoading
+  isLoading,
 }: PromptProps) {
-  console.log('input in prompt form:', input)
-  const { formRef, onKeyDown } = useEnterSubmit()
-  const inputRef = React.useRef<HTMLTextAreaElement>(null)
+  console.log('input in prompt form:', input);
+  const { formRef, onKeyDown } = useEnterSubmit();
+  const inputRef = React.useRef<HTMLTextAreaElement>(null);
 
   React.useEffect(() => {
     if (inputRef.current) {
-      inputRef.current.focus()
+      inputRef.current.focus();
     }
-  }, [])
+  }, []);
 
   return (
     <form
       onSubmit={async e => {
-        e.preventDefault()
+        e.preventDefault();
         if (input === '') {
-          return
+          return;
         }
-        setInput('')
-        await onSubmit(input)
+        setInput('');
+        await onSubmit(input);
       }}
       ref={formRef}
     >
-      <div className="relative flex w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
@@ -91,5 +91,5 @@ export function PromptForm({
         </div>
       </div>
     </form>
-  )
+  );
 }
