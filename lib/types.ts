d61cb072f992a1a7a8f2e1325616e22decc6368f
KimboTodd <kimtoddsotheremail@gmail.com>
2023-06-28 13:00:50 -0400

    create character type

--- lib/types.ts
+++ lib/types.ts
@@ -1,18 +1,78 @@
-import { type Message } from 'ai'
+import { type Message } from 'ai';
 
 export interface Chat extends Record<string, any> {
-  id: string
-  title: string
-  createdAt: Date
-  userId: string
-  path: string
-  messages: Message[]
-  sharePath?: string
+  id: string;
+  title: string;
+  createdAt: Date;
+  userId: string;
+  path: string;
+  messages: Message[];
+  sharePath?: string;
 }
 
 export type ServerActionResult<Result> = Promise<
   | Result
   | {
-      error: string
+      error: string;
     }
->
+>;
+
+export interface Character extends Record<string, any> {
+  id?: string;
+  sharePath?: string;
+  userId: string;
+  createdAt: Date;
+  name: string;
+  race: string;
+  class: string;
+  alignment: string;
+  bio: string;
+  appearance: string;
+  abilities: {
+    strength: {
+      base: number;
+      modifier: string;
+    };
+    dexterity: {
+      base: number;
+      modifier: string;
+    };
+    constitution: {
+      base: number;
+      modifier: string;
+    };
+    intelligence: {
+      base: number;
+      modifier: string;
+    };
+    wisdom: {
+      base: number;
+      modifier: string;
+    };
+    charisma: {
+      base: number;
+      modifier: string;
+    };
+  };
+  proficiencies: {
+    savingThrows: {
+      intelligence: number;
+      wisdom: number;
+    };
+    skills: {
+      arcana: number;
+      history: number;
+      investigation: number;
+      perception: number;
+    };
+    armor: string;
+    weapons: string;
+    tools: string;
+    languages: string;
+  };
+  spells: {
+    cantrips: string[];
+    firstLevel: string[];
+  };
+  equipment: string[];
+}
